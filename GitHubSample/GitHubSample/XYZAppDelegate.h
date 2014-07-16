cecffba610a3407f939a0bb36e960849105038a8
greghein <greghein@Gregs-iMac-3.home>
2014-07-15 19:55:34 -0700

    Our initial commit
    Created Xcode project

---
+++ GitHubSample/GitHubSample/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  GitHubSample
+//
+//  Created by Greg Heinrichs on 7/15/14.
+//  Copyright (c) 2014 Greg Heinrichs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubSample/GitHubSample/XYZViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZViewController.h
+//  GitHubSample
+//
+//  Created by Greg Heinrichs on 7/15/14.
+//  Copyright (c) 2014 Greg Heinrichs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZViewController : UIViewController
+
+@end
